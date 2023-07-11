57ec0f78b95b70a5be752938b939f8137f88f5fe
Fernanda Vidal <65190924+ifdotcom@users.noreply.github.com>
2023-07-11 11:52:00 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,29 +1,17 @@
 // See https://github.com/typicode/json-server#module
-// se agrego fs:
-const fs = require('fs')
-// se agregó path:
-const path = require('path')
-//se agrego auth:
-const auth = require('json-server-auth')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-// se agrego port:
-const port = process.env.PORT || 8080
-//se agrego rules
-const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-// /!\ Bind the router db to the app
-app.db = router.db
 
 server.use(middlewares)
 // Add this before server.use(router)
-// se modificó:
-server.use(rules)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
-// Se agregó
-app.use(auth)
-server.listen(port, () => {
+server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
