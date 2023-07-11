5fadfdfa7b2ebb3fd453141733b5f8d02b4f2bdd
Fernanda Vidal <65190924+ifdotcom@users.noreply.github.com>
2023-07-11 13:56:06 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,24 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const jsonServer = require('json-server');
-const auth = require('json-server-auth');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-const port = process.env.PORT || 8080;
-
-const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// /!\ Bind the router db to the app
-app.db = router.db
-
-// You must apply the auth middleware before the router
-app.use(middlewares);
-app.use(rules);
-app.use(auth);
-app.use(router);
-app.listen(port, () => {
-    console.log(`JSON Server is running in ${port}`);
-});
+// Export the Server API
+module.exports = server
